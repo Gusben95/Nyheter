50feec53539f08b5e5a63478021624c176737d5d
Jacob Klaren <33457831+Kybbe@users.noreply.github.com>
2022-09-05 12:57:32 +0200

    Added article, did template for homepage and so on.

--- client/src/views/Admin.jsx
+++ client/src/views/Admin.jsx
@@ -1,6 +1,8 @@
+import styles from './Admin.module.css'
+
 export default function Admin() {
   return (
-    <div className="adminpage">
+    <div className={styles.adminpage}>
       <h1>Här ska man kunna lägga till artiklar o sånt kanske</h1>
     </div>
   )
