7f5684d0ba1003b859e32e172bf688735f8c8262
Gustav Bengtsson <bengtssongustav95@gmail.com>
2022-09-08 13:48:20 +0200

    Update App.test.js

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,19 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-
+import {
+  ArticleComp, 
+} from '../../client/src/components/Article/ArticleComp';
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+
+
+describe('article comp titel', () => {
+  test('check if titel returns a string with a titel', () => {
+    expect(typeof ArticleComp.titel).toBe(string);
+  });
+});localStorage
