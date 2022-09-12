7366667434dd9284700956f7d9ad9fda98097052
Mattegbg <90768274+Mattegbg@users.noreply.github.com>
2022-09-12 11:47:41 +0200

    subscribe site costs and info

--- client/src/views/Subscribe/Subscribe.jsx
+++ client/src/views/Subscribe/Subscribe.jsx
@@ -1,7 +1,12 @@
+import { useState } from 'react'
 import styles from './Subscribe.module.css'
 
 export default function Subscribe() {
+
+const [clickedMonths, setClickedMonths] = useState()
+
   function clickSubscribe(months){
+    setClickedMonths(months)
     console.log(months)
 
   }
@@ -15,6 +20,26 @@ export default function Subscribe() {
       <div className={styles.SubscribeCard} onClick={()=>{clickSubscribe(6)}}><h2>6 Månader: 500Sek</h2></div>
       <div className={styles.SubscribeCard} onClick={()=>{clickSubscribe(12)}}><h2>12 Månader: 800Sek</h2><p>Mest prisvärda alternativet!</p></div>
 
+      
+      {clickedMonths ? (<section>
+        <input type='text' placeholder='För och efternamn'></input>
+        
+        <input type='number' placeholder='Ditt kortnummer'></input>
+        <input type='text' placeholder='YY/MM'></input>
+        <input type='number' placeholder='CVC'></input>
+        <button>Betala</button>
+
+
+      </section>):''}
+
+
+      <ul>
+        <h3>Vad ingår i abbonnemanget</h3>
+        <li>Tillgång till fullständiga artiklar</li>
+        <li>Reklamfritt</li>
+        <li>Välj vilka artiklar du ser</li>
+        <li>Bli prioriterad i kundtjänsten</li>
+      </ul>
 
     </section>
   )
