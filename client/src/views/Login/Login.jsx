698c0e2b6b8dfbd9edc99efcf0e026e88c752a84
Mattegbg <90768274+Mattegbg@users.noreply.github.com>
2022-09-15 14:28:59 +0200

    css loginpage & footer
    buttons, rounded corners, simple colors of buttons, hoovers, shadows etc. Light fix on the Footer with color and commented out the ''tipslåda''

--- client/src/views/Login/Login.jsx
+++ client/src/views/Login/Login.jsx
@@ -27,18 +27,19 @@ export default function Login(){
 
   function appleLogin() {
     console.log('inloggad med apple');
+    alert('inloggad med apple');
+
 
   }
 
   function googleLogin() {
     console.log('inloggad med google');
-
+    alert('inloggad med google');
   }
 
 
 return(
   <div className={styles.loginContainer}>
-    <fieldset className={styles.LoginPage}>   
       <legend>Logga in</legend>
 
       <label htmlFor='uname'>Email</label>
@@ -56,7 +57,6 @@ return(
 
       <button className='apple' onClick={appleLogin}>Logga in med Apple</button>
       <button className='google' onClick={googleLogin}>Logga in med Google</button>
-    </fieldset>
 
 
 
