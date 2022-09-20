7b6b08231b681f1699f89673635492b448c3c564
Mattegbg <90768274+Mattegbg@users.noreply.github.com>
2022-09-20 14:25:20 +0200

    footer CSS inte helt klar

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -5,25 +5,40 @@ import styles from './Footer.module.css'
 
 export default function Footer() {
 
-    return(
+    return (
         <footer className={styles.footer}>
             {/* <h2> Tipslåda</h2>
 				Här kan du skicka in tips annonymt om saker i samhället som du anser bör rapporteras. <br/>
         <textarea /> <br/>
         <button>skicka tipset</button> */}
 
-        <h2>Kontakta oss. </h2>
-        <ul>
-            <li>
-                Adress: Järntorget 1. 413 14, Göteborg.
-            </li>
-            <li>
-                Telefonnummer: 12345678910 - Öppettider 10-16 vardagar. 
-            </li>
-            <li>
-                Mailadress: jacob.klaren@nyhetssida.se
-            </li>
-        </ul>
+            <h2>Nyhetssidan</h2>
+
+            <h4>Get in touch</h4>
+
+            <div className={styles.footerLinks}>
+                <div className={styles.footerLinkLeft}>
+                    <p>Om oss</p>
+                    <p>Hjälp</p>
+                    <p>Annonsera</p>
+                </div>
+                <div className={styles.footerLinkRight}>
+                    <p>Kontakta oss</p>
+                    <p>Korrigeringar</p>
+                    <p>Karriär</p>
+                </div>
+                <div className={styles.footerLinkMedia}>
+
+                </div>
+            </div>
+
+            <p>Adress: Järntorget 1. 413 14, Göteborg.</p>
+            <p>Telefonnummer: 12345678910 - Öppettider 10-16 vardagar.</p>
+            <p>Mailadress: jacob.klaren@nyhetssida.se</p>
+
+
+
+
         </footer>
     )
 }
