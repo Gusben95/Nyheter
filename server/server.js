a9f9fb27969856dbed8337ffb46ad3a21da40ee2
Jacob Klaren <33457831+Kybbe@users.noreply.github.com>
2022-09-22 13:37:07 +0200

    updated server to allow CORS stuff for live version

--- server/server.js
+++ server/server.js
@@ -31,6 +31,28 @@ init().then(initAcc().then(() => {
   app.listen(PORT);
 }))
 
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  res.setHeader('cross-origin-resource-policy', 'cross-origin');
+
+  // Pass to next layer of middleware
+  next();
+});
+
 // -------- article database --------
 // Get all the articles from the database.
 // Loops through the articles the places them in an array
