b29f2984361b4f4bc3cb0c649a07b9c7577b50c9
Luken95 <Lukass_basket@hotmail.com>
2022-09-22 11:37:19 +0200

    hello

--- client/src/views/Login/Login.jsx
+++ client/src/views/Login/Login.jsx
@@ -1,8 +1,13 @@
 import { Link } from 'react-router-dom'
 import styles from './Login.module.css'
-import { useRef } from 'react'
+import { useRef,useEffect } from 'react'
+import { GoogleLogin } from 'react-google-login';
+import { gapi } from 'gapi-script';
+
 
 const { fetchAccountWithEmail } = require('../../dbUtils/accountActions')
+const clientId = '299303035876-kus8sfr8h4e38iape0ivksrarjqmouef.apps.googleusercontent.com';
+
 
 export default function Login(){
   //keep reference to inputs in HTML.
@@ -26,10 +31,23 @@ export default function Login(){
     alert('inloggad med apple');
   }
 
-  function googleLogin() {
-    console.log('inloggad med google');
-    alert('inloggad med google');
-  }
+useEffect(() => {
+     const initClient = () => {
+           gapi.client.init({
+           clientId: clientId,
+           scope: ''
+         });
+      };
+      gapi.load('client:auth2', initClient);
+  }, []);
+
+  const onSuccess = (res) => {
+    console.log('success:', res);
+};
+const onFailure = (err) => {
+    console.log('failed:', err);
+};
+
 
 return(
   <div className={styles.loginContainer}>
@@ -52,9 +70,16 @@ return(
         </span>
       </div>
 
-      <button className='apple' onClick={appleLogin}>Logga in med Apple</button>
-      <button className='google' onClick={googleLogin}>Logga in med Google</button>
+      <GoogleLogin
+      clientId={clientId}
+      buttonText="Sign in with Google"
+      onSuccess={onSuccess}
+      onFailure={onFailure}
+      cookiePolicy={'single_host_origin'}
+      isSignedIn={true}
+  />
 
+      <button className='apple' onClick={appleLogin}>Logga in med Apple</button>
       <Link to="/prenumerera">Bli Prenumerant</Link>
   </div>
 )}
