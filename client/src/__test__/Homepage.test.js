9ea8f613794d62f95941ffd523e054a442861def
VivianTang11 <vivian.tang@hotmail.se>
2022-09-23 16:17:57 +0200

    Added loading/article and scrollToTop tests

--- client/src/__test__/Homepage.test.js
+++ client/src/__test__/Homepage.test.js
@@ -5,28 +5,39 @@ import { BrowserRouter } from 'react-router-dom';
 import Homepage from '../views/Homepage/Homepage';
 
 const MockHomepage = () => {
-
   return (
-      <Provider store={store}>
-        <BrowserRouter>
-          <Homepage/>
-        </BrowserRouter>
-      </Provider>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Homepage/>
+      </BrowserRouter>
+    </Provider>
   )
 };
 
 describe('Homepage', () => {
   it('should redirect to "/prenumerera" when link is clicked', () => {
-    render(<MockHomepage/>)
-    const linkElement = screen.getByRole('link', {name: /prenumerera nu/i})
-    fireEvent.click(linkElement)
-    expect(windows.location.href).toBe("http://localhost/prenumerera")
+    render(<MockHomepage/>);
+    const linkElement = screen.getByRole('link', {name: /prenumerera nu/i});
+    fireEvent.click(linkElement);
+    expect(window.location.href).toBe("http://localhost/prenumerera");
   })
   it('should redirect to "/prenumerera" when link is clicked', () => {
     render(<MockHomepage/>)
     const linkElement = screen.getByRole('link', {name: /logga in/i})
     fireEvent.click(linkElement)
-    expect(windows.location.href).toBe("http://localhost/login")
+    expect(window.location.href).toBe("http://localhost/login")
+  })
+  it('should render loading or articles',( )=> { /* hur articles???*/
+    render(<MockHomepage/>)
+    const loading = screen.getByText(/loading.../i)
+    expect(loading).toBeTruthy()
+  })
+  it('should go back to top when ⬆️ is clicked', () => {
+    window.scrollTo = jest.fn()
+    render(<MockHomepage/>)
+    const backToTop = screen.getByText(/Tillbaka till toppen/i)
+    fireEvent.click(backToTop)
+    expect(window.scrollTo).toBeCalledWith({"behavior": "smooth", "left": 0, "top": 0})
   })
 })
 
