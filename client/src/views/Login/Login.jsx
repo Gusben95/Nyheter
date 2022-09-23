32e2d58268be404ce20f7d78385e8b526522141d
VivianTang11 <vivian.tang@hotmail.se>
2022-09-23 16:28:52 +0200

    .

--- client/src/views/Login/Login.jsx
+++ client/src/views/Login/Login.jsx
@@ -1,48 +1,31 @@
-import { Link, useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { useRef } from 'react'
 import styles from './Login.module.css'
-import { useRef,useEffect } from 'react'
-import { GoogleLogin } from 'react-google-login';
-import { gapi } from 'gapi-script';
 import { useDispatch, useSelector } from 'react-redux'
 
-
 const { fetchAccountWithEmail } = require('../../dbUtils/accountActions')
-/* göm med env */
-const clientId = '299303035876-kus8sfr8h4e38iape0ivksrarjqmouef.apps.googleusercontent.com';
-
 
 export default function Login(){
+
   const stateUser = useSelector(state => state.User)
   const dispatch = useDispatch();
-  const navigate = useNavigate();
-  
+
   //keep reference to inputs in HTML.
   const emailInput = useRef('');
   const passwordInput = useRef('');
 
   //Login in user
-  async function loginAuth(googleLoginAuth){
+  async function loginAuth(){
     const account = {
       email: emailInput.current.value,
       password: passwordInput.current.value
-    } 
-    if (googleLoginAuth){
-      account.email = googleLoginAuth;
     }
-    
 
     const accountInfo = await fetchAccountWithEmail(account)
     console.log(accountInfo)
 
     if(accountInfo?.email) {
       dispatch({type: "setUser", data: accountInfo})
-
-      if(accountInfo?.role === "admin") {
-        // eslint-disable-next-line no-restricted-globals
-        if(confirm("Admin logged in, redirect to admin page?")) {
-          navigate('/admin')
-        }
-      }
     } else {
       alert("Wrong email or password")
     }
@@ -53,36 +36,16 @@ export default function Login(){
     alert('inloggad med apple');
   }
 
-useEffect(() => {
-     const initClient = () => {
-           gapi.client.init({
-           clientId: clientId,
-           scope: ''
-         });
-      };
-      gapi.load('client:auth2', initClient);
-  }, []);
-
-  const onSuccess = (res) => {
-    let email = res.profileObj.email;
-    let name = res.profileObj.name;
-    console.log(name)
-    console.log('success:', res);
-};
-const onFailure = (err) => {
-    console.log('failed:', err);
-};
-
-function linkToHomepage(){
-  navigate('/')
-}
-
+  function googleLogin() {
+    console.log('inloggad med google');
+    alert('inloggad med google');
+  }
 
   let subscriptionEndFormatted = new Date(stateUser.subscriptionEnd).toLocaleDateString('sv-SE', {year: 'numeric', month: 'long', day: 'numeric'});
 
   return(
     <div className={styles.loginContainer}>
-      <h1 onClick={linkToHomepage} >Nyhetssidan</h1>
+      <h1>Nyhetssidan</h1>
 
       { stateUser.email ? (
         <section className={styles.loggedIn}>
@@ -114,18 +77,12 @@ function linkToHomepage(){
             </span>
           </div>
 
-      <GoogleLogin
-      clientId={clientId}
-      buttonText="Sign in with Google"
-      onSuccess={onSuccess}
-      onFailure={onFailure}
-      cookiePolicy={'single_host_origin'}
-      isSignedIn={true}
-      />
+          <button className='apple' onClick={appleLogin}>Logga in med Apple</button>
+          <button className='google' onClick={googleLogin}>Logga in med Google</button>
 
-      <button className='apple' onClick={appleLogin}>Logga in med Apple</button>
-      <Link to="/prenumerera">Bli Prenumerant</Link>
-      </section>
+          <Link to="/prenumerera">Bli Prenumerant</Link>
+        </section>
       )}
-  </div>
-)}
+    </div>
+  )
+}
