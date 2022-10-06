72a2f01254dd6fd69f94882b83f6d2ea457f2e83
Gustav Bengtsson <bengtssongustav95@gmail.com>
2022-10-06 12:00:40 +0200

    Added test

---
+++ client/src/__test__/ad.test.jsx
@@ -0,0 +1,14 @@
+import {ads, randomizedAdd} from '../components/Ad/Ad';
+
+describe('arrayContaining', () => {
+    it('Ads should contain atleast 3 files', async () => {
+        expect(ads.length).toBeLessThan(4);
+       
+      })
+        it('Ads should contain atleast one image', async () => {
+            expect(ads.length).toBeGreaterThan(0);
+        
+          })
+      });
+  
+  
